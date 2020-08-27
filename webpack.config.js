14255d661d476e72fe6b5b39b463e868491c7a4a
Zeeshan Zakariya <31367405+darkraken007@users.noreply.github.com>
2020-08-27 21:08:14 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
+
+module.exports = {
+  output: {
+    // `filename` provides a template for naming your bundles (remember to use `[name]`)
+    filename: '[name].bundle.js',
+    // `chunkFilename` provides a template for naming code-split bundles (optional)
+    chunkFilename: '[name].bundle.js',
+  },
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({})],
+    splitChunks: {
+			cacheGroups: {
+				commons: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all'
+				}
+			}
+		},
+  },
+  devServer: {
+    historyApiFallback: true,
+    port:3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(svg|webp|woff|woff2|ttf|png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath:'images',
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./main.html"
+    })
+     // new BundleAnalyzerPlugin(),
+  ],
+  externals:{
+    react:"React",
+    "react-dom":"ReactDOM"
+  }
+};
