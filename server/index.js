f9136873379318a1cd7d293d3518b21e6995092b
Zeeshan Zakariya <31367405+darkraken007@users.noreply.github.com>
2020-08-29 21:02:55 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -4,34 +4,17 @@ const URLSearchParams = require('url');
 const path = require('path'); 
 const { get } = require('http');
 const app = express();
-var JsonDB = require("node-json-db").JsonDB;
-var Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
-var cron = require('node-cron');
+
 
 const port = process.env.PORT || 80;
 const DIST_DIR = path.join(__dirname, '../dist'); 
 const HTML_FILE = path.join(DIST_DIR, 'main.html'); 
-const DB_FILE = path.join(__dirname, '../myDataBase.json'); 
 
 
-var db = new JsonDB(new Config("myDataBase",true,false,'/'));
+
+
 
  
-cron.schedule('* * 1 * *', () => {
-  console.log('resetting monthly count');
-  db.push("/monthlyCount",0);
-
-});
-
-cron.schedule('* 0 * * *', () => {
-  console.log('resetting daily count');
-  db.push('/dailyCount',0);
-});
-
-cron.schedule('* 0 * * *', () => {
-  console.log('resetting yearly count');
-  db.push('/yearlyCount',0);
-});
 
 
 app.use(express.static(DIST_DIR));
@@ -41,6 +24,54 @@ const mockResponse = {
   bar: 'foo'
 };
 
+const encodeFormData = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&');
+}
+function checkAndSendResponse(data,res){
+  if(data.message=="Record updated."){
+    res.sendStatus(200);
+  }
+  else{
+    res.sendStatus(403);
+  }
+}
+function updateAndSend(data,count,res){
+  var d = new Date();
+  var body="";
+  if(count==-1){
+    body = {
+      dailyCount: 0,
+      monthlyCount: 0,
+      yearlyCount: 0,
+      averageCountPerDay: 0,
+      totalCount: 0
+    }
+  }
+  else{
+    body = {
+    dailyCount: data[0].dailyCount+count,
+    monthlyCount: data[0].monthlyCount+count,
+    yearlyCount: data[0].yearlyCount+count,
+    averageCountPerDay: +(((data[0].monthlyCount+count)/d.getDate()).toFixed(2)),
+    totalCount: data[0].totalCount+count
+  }
+}
+  var url="https://jsonbox.io/box_e1bbe0030864cee81568/5f4a3c5d9b2b9c00179906df";
+  const requestOptions = {
+      method: 'PUT',
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json'
+    }
+  };
+
+  fetch(url, requestOptions)
+  .then(response=>response.json())
+  .then(resJson=>checkAndSendResponse(resJson,res))
+  .catch(error=>res.sendStatus(500))
+
+}
 
 
 app.get('/update', (req, res) => {
@@ -55,18 +86,16 @@ app.get('/update', (req, res) => {
         res.sendStatus(403)
       }
       var count = parseInt(req.query.count);
-      var dailycount = db.getData("/dailyCount") | 0;
-      var monthlycount = db.getData("/monthlyCount") | 0;
-      var yearlycount = db.getData("/yearlyCount") | 0;
-      var totalCount = db.getData('/totalCount') | 0;
-      var averageCountPerDayThisMonth = db.getData("/averageCountPerDay") | 0;
-      db.push("/dailyCount",dailycount+count);
-      db.push("/monthlyCount",monthlycount+count);
-      db.push("/yearlyCount",yearlycount+count);
-      db.push("/totalCount",totalCount+count);
-      db.push("/averageCountPerDay",+(monthlycount/date.getDate()).toFixed(2));
+
+      var url="https://jsonbox.io/box_e1bbe0030864cee81568";
+      const requestOptions = {
+            method: 'GET'
+        };
+   fetch(url, requestOptions)
+   .then(response=>response.json())
+   .then(data=>updateAndSend(data,count,res))
+   .catch(error=>res.sendStatus(500))
     }
-    res.sendStatus(200);
 
   }
 });
@@ -75,26 +104,30 @@ app.get('/', (req, res) => {
   res.sendFile(HTML_FILE);
 });
 
+function sendPrunedJson(data,res){
+  var pruned_json = {
+    "dailyCount":data[0].dailyCount,
+    "monthlyCount":data[0].monthlyCount,
+    "yearlyCount":data[0].yearlyCount,
+    "averageCountPerDay":data[0].averageCountPerDay,
+    "totalCount":data[0].totalCount
+  };
+  res.send(pruned_json);
+}
+
+
 app.get('/getStat', (req, res) => {
-  res.sendFile(DB_FILE);
+  var url="https://jsonbox.io/box_e1bbe0030864cee81568";
+    const requestOptions = {
+        method: 'GET'
+    };
+    fetch(url, requestOptions)
+    .then(response=>response.json())
+    .then(data=>sendPrunedJson(data,res))
+    .catch(error=>res.sendStatus(500))
+
 });
 
-
-app.get('/reset', (req, res) => {
-  
-  if(req.query.key==""||req.query.key==undefined){
-    res.sendStatus(200);
-  }
-  else{
-    if(req.query.key=="854317221"){
-      db.push("/dailyCount",0);
-      db.push("/monthlyCount",0);
-      db.push("/yearlyCount",0);
-      db.push("/totalCount",0);
-      db.push("/averageCountPerDay",0);
-    }}
-    res.sendStatus(200);
-  });
 
 app.listen(port, function () {
  console.log('App listening on port: ' + port);
